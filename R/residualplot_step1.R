29eac6a05ca18d797898b4b2b25a5c048185824a
Hannah Lennon <hannah.lennon.manchester@gmail.com>
2019-11-08 16:51:09 +0100

    Update to residual_plot from user

--- R/residualplot_step1.R
+++ R/residualplot_step1.R
@@ -26,88 +26,121 @@
 #' )
 #' @export
 
-residualplot_step1 <- function(model, nameofoutcome="bmi",  nameofage = "age", data = bmi_long,
-                                                   ylimit=c(-5,5)){
-
-                                        require(dplyr)
-                                        k     <- model$ng
-                                        preds <- model$pred
-                                        names(preds)[6] <- nameofoutcome
-                                        nameofid        <- names(model$pred)[1]
-                                        test <- dplyr::left_join(preds, model$pprob, .by=nameofid)
-                                        test <- dplyr::left_join(test, bmi_long, .by=c(nameofid, nameofoutcome))
-
-                                        plotvalues <- NULL
-
-                                        for(i in 1:k){
-
-                                                            newplotvalues <- test %>% filter(class==i) %>% mutate(Residuals=get(nameofoutcome)-eval(parse(text=paste0("pred_ss",i))))
-                                                            plotvalues <- rbind(plotvalues, newplotvalues)
-
-                                                            plotvaluessub <- plotvalues %>% filter(class==i)
-
-
-
-                                                            pname <- paste0("p", i)
-                                                            assign(pname,  ggplot2::ggplot(data = plotvaluessub, aes(x = age, y = Residuals, group = class))+
-                                                                                       theme(axis.text=element_text(size=16),text = element_text(size=16)) +
-                                                                                       geom_point() +
-                                                                                       stat_summary(fun.y=mean, geom="line", size = 3, col="CadetBlue", group=1) +
-                                                                                       ggtitle("Residuals in class", i) +
-                                                                                       ylim(ylimit))
-                                                            print(eval(parse(text=(paste0("p",i)))))
-                                        plotname <- paste0("p",i, ".png")
-                                        ggplot2::ggsave(filename=plotname)
-
-                                        }
-                                         # print(ggpubr::ggarrange(eval(parse(text=(paste0("p",1:k)))), width=1, ncol = k))
-                                         return( as.list(get(dput(paste0("p",1:k)) )))
-                                        }
+residualplot_step1 <- function (model, nameofoutcome, nameofage, data, 
+            ylimit = c(-50, 50)) 
+{
+                    require(dplyr)
+                    k <- model$ng
+                    preds <- model$pred
+                    names(preds)[6] <- nameofoutcome
+                    nameofid <- names(model$pred)[1]
+                    test <- dplyr::left_join(preds, model$pprob, .by = nameofid)
+                    test <- dplyr::left_join(test, data, .by = c(nameofid, 
+                                                                 nameofoutcome))
+                    plotvalues <- NULL
+                    for (i in 1:k) {
+                                        newplotvalues <- test %>% filter(class == i) %>% mutate(Residuals = get(nameofoutcome) - 
+                                                                                                                    eval(parse(text = paste0("pred_ss", i))))
+                                        plotvalues <- rbind(plotvalues, newplotvalues)
+                                        plotvaluessub <- plotvalues %>% filter(class == i)
+                                        pname <- paste0("p", i)
+                                        assign(pname, ggplot2::ggplot(data = plotvaluessub, aes(x = get(nameofage), 
+                                                                                                y = Residuals, group = class)) + theme(axis.text = element_text(size = 16), 
+                                                                                                                                       text = element_text(size = 16)) + geom_point() + stat_summary(fun.y = mean, geom = "line", size = 3, 
+                                                                                col = "CadetBlue", group = 1) + ggtitle("Residuals in class", 
+                                                                                                                        i) + ylim(ylimit))
+                                        print(eval(parse(text = (paste0("p", i)))))
+                                        plotname <- paste0("p", i, ".png")
+                                        ggplot2::ggsave(filename = plotname)
+                    }
+                    return(as.list(get(dput(paste0("p", 1:k)))))
+}
 
 
-
+# 
+# 
 # residualplot_step1 <- function(model, nameofoutcome="bmi",  nameofage = "age", data = bmi_long,
-                               # type = "line", ylimit=c(-5,5)){
-#   k <- model$ng
-#   preds <- model$pred
-#   names(preds)[6] <- nameofoutcome
-#   nameofid <- names(model$pred)[1]
-#   names(data)[names(data) == nameofage] <- "ID"
-#   nameofage <- names(data)[names(data) == "ID"]
-#
-#
-#   test <- dplyr::left_join(preds, model$pprob, .by = nameofid)
-#   test <- dplyr::left_join(test, data, .by = c(nameofid, nameofoutcome))
-#   test <- test %>%
-#     group_by(class) %>%
-#     mutate(Std_resid = resid_ss / sqrt((1 / (length(resid_ss) - 1)) * sum(resid_ss^2)))
-#
-#   library(ggplot2)
-#
-#   if (type != "point") {
-#     p <- ggplot(
-#       data = test,
-#       aes_string(x = nameofage, y = "Std_resid", group = nameofid)
-#     ) +
-#       geom_line(alpha = 0.3) +
-#       geom_smooth(
-#         mapping = aes_string(x = nameofage, y = "Std_resid", group = NULL),
-#         method = "loess", colour = "CadetBlue", size = 1.2
-#       ) +
-#       labs(x = "ID", y = "Standardised residuals") +
-#       facet_wrap(~class)
-#   } else {
-#     p <- ggplot(
-#       data = test,
-#       aes_string(x = nameofage, y = "Std_resid", group = nameofid)
-#     ) +
-#       geom_point(alpha = 0.7) +
-#       geom_smooth(
-#         mapping = aes_string(x = nameofage, y = "Std_resid", group = NULL),
-#         method = "loess", colour = "CadetBlue", size = 1.2
-#       ) +
-#       labs(x = "ID", y = "Standardised residuals") +
-#       facet_wrap(~class)
-#   }
-#   p
-# }
+#                                                    ylimit=c(-5,5)){
+# 
+#                                         require(dplyr)
+#                                         k     <- model$ng
+#                                         preds <- model$pred
+#                                         names(preds)[6] <- nameofoutcome
+#                                         nameofid        <- names(model$pred)[1]
+#                                         test <- dplyr::left_join(preds, model$pprob, .by=nameofid)
+#                                         test <- dplyr::left_join(test, bmi_long, .by=c(nameofid, nameofoutcome))
+# 
+#                                         plotvalues <- NULL
+# 
+#                                         for(i in 1:k){
+# 
+#                                                             newplotvalues <- test %>% filter(class==i) %>% mutate(Residuals=get(nameofoutcome)-eval(parse(text=paste0("pred_ss",i))))
+#                                                             plotvalues <- rbind(plotvalues, newplotvalues)
+# 
+#                                                             plotvaluessub <- plotvalues %>% filter(class==i)
+# 
+# 
+# 
+#                                                             pname <- paste0("p", i)
+#                                                             assign(pname,  ggplot2::ggplot(data = plotvaluessub, aes(x = age, y = Residuals, group = class))+
+#                                                                                        theme(axis.text=element_text(size=16),text = element_text(size=16)) +
+#                                                                                        geom_point() +
+#                                                                                        stat_summary(fun.y=mean, geom="line", size = 3, col="CadetBlue", group=1) +
+#                                                                                        ggtitle("Residuals in class", i) +
+#                                                                                        ylim(ylimit))
+#                                                             print(eval(parse(text=(paste0("p",i)))))
+#                                         plotname <- paste0("p",i, ".png")
+#                                         ggplot2::ggsave(filename=plotname)
+# 
+#                                         }
+#                                          # print(ggpubr::ggarrange(eval(parse(text=(paste0("p",1:k)))), width=1, ncol = k))
+#                                          return( as.list(get(dput(paste0("p",1:k)) )))
+#                                         }
+# 
+# 
+# 
+# # residualplot_step1 <- function(model, nameofoutcome="bmi",  nameofage = "age", data = bmi_long,
+#                                # type = "line", ylimit=c(-5,5)){
+# #   k <- model$ng
+# #   preds <- model$pred
+# #   names(preds)[6] <- nameofoutcome
+# #   nameofid <- names(model$pred)[1]
+# #   names(data)[names(data) == nameofage] <- "ID"
+# #   nameofage <- names(data)[names(data) == "ID"]
+# #
+# #
+# #   test <- dplyr::left_join(preds, model$pprob, .by = nameofid)
+# #   test <- dplyr::left_join(test, data, .by = c(nameofid, nameofoutcome))
+# #   test <- test %>%
+# #     group_by(class) %>%
+# #     mutate(Std_resid = resid_ss / sqrt((1 / (length(resid_ss) - 1)) * sum(resid_ss^2)))
+# #
+# #   library(ggplot2)
+# #
+# #   if (type != "point") {
+# #     p <- ggplot(
+# #       data = test,
+# #       aes_string(x = nameofage, y = "Std_resid", group = nameofid)
+# #     ) +
+# #       geom_line(alpha = 0.3) +
+# #       geom_smooth(
+# #         mapping = aes_string(x = nameofage, y = "Std_resid", group = NULL),
+# #         method = "loess", colour = "CadetBlue", size = 1.2
+# #       ) +
+# #       labs(x = "ID", y = "Standardised residuals") +
+# #       facet_wrap(~class)
+# #   } else {
+# #     p <- ggplot(
+# #       data = test,
+# #       aes_string(x = nameofage, y = "Std_resid", group = nameofid)
+# #     ) +
+# #       geom_point(alpha = 0.7) +
+# #       geom_smooth(
+# #         mapping = aes_string(x = nameofage, y = "Std_resid", group = NULL),
+# #         method = "loess", colour = "CadetBlue", size = 1.2
+# #       ) +
+# #       labs(x = "ID", y = "Standardised residuals") +
+# #       facet_wrap(~class)
+# #   }
+# #   p
+# # }
